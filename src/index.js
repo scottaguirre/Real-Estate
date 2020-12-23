cd5b5221e21893c6853e3a60b98ae9762aadcbfc
Edwin Scott <scottaguirre@yahoo.com>
2020-12-23 11:53:23 -0600

    Real Estate WordPress Site

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles/styles.scss';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import './js';
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import { menu, carousel, initialOverlayEffect } from './components';
+
+initialOverlayEffect();
+menu();
+carousel();
