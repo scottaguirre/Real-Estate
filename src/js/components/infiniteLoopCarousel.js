1b915073c744bc9274c1d60380d5d1d419c929c0
Edwin Scott <scottaguirre@yahoo.com>
2021-01-11 16:18:35 -0600

    JavaScript updtared for slider with left and right arrows

--- src/js/components/infiniteLoopCarousel.js
+++ src/js/components/infiniteLoopCarousel.js
@@ -1,60 +1,94 @@
 const carousel = () => {
-  // get the width of the image container
   let carousel_item_img = document.querySelectorAll('.carousel-item-img');
   let carousel_featured_properties = document.querySelector(
     '.carousel-featured-properties'
   );
+  let leftArrow = document.querySelector('.left-arrow');
+  let rightArrow = document.querySelector('.right-arrow');
   let slideTransitionDuration = null;
 
+  //==== Function ====//
+  // this function gets the width of the image container including the margins
   const gettingContainerImgWidth = () => {
     let containerImageTotalWidth = null;
     // getting the margin of an element
     let style = getComputedStyle(carousel_item_img[0]);
     let marginLeft = parseInt(style.marginLeft);
     let marginRight = parseInt(style.marginRight);
-
     // getting the offset of an element; including content, border, padding but not margin
     let offsetImage = carousel_item_img[0].offsetWidth;
-
-    // result of adding content + margin
+    // The result of adding content + margin
     containerImageTotalWidth = offsetImage + marginLeft + marginRight;
     return containerImageTotalWidth;
   };
 
-  // adding translate values and class to move container-image to the left every 3 seconds
+  //===== Function ====//
+  // Adding transitionend EventListener and removing transitionend EventListener
+  let addingTransitionEndAndRemovingTransitionEnd = () => {
+    // 1. Declaring the function that's triggered when the transition ends.
+    let appendContainerImg = () => {
+      carousel_featured_properties.style.transition = `none`;
+      carousel_featured_properties.style.transform = `translateX(0)`;
+      carousel_featured_properties.appendChild(
+        carousel_featured_properties.firstElementChild
+      );
+    };
 
+    // 2. Adding transitionend Eventlistener
+    carousel_featured_properties.addEventListener(
+      'transitionend',
+      appendContainerImg
+    );
+
+    // 3. Removing transitioned Eventlistener when the transition ends
+    setTimeout(function () {
+      carousel_featured_properties.removeEventListener(
+        'transitionend',
+        appendContainerImg
+      );
+      // 4. adding transition property back to the element
+      carousel_featured_properties.style.transition = `all 0.6s linear`;
+    }, 650);
+  };
+
+  //=== Function =====//
+  // Adding translate values to move container-image to the left every 3 seconds
   const runningCarousel = () => {
     slideTransitionDuration = setInterval(() => {
       // 1. Starting the transition
       carousel_featured_properties.style.transform = `translateX(-${gettingContainerImgWidth()}px)`;
-
-      // 2. Declaring the function that's triggered when the transition end.
-      let appendContainerImg = () => {
-        carousel_featured_properties.style.transition = `none`;
-        carousel_featured_properties.style.transform = `translateX(0)`;
-        carousel_featured_properties.appendChild(
-          carousel_featured_properties.firstElementChild
-        );
-      };
-
-      // 3. Adding transitioned eventlistener
-      carousel_featured_properties.addEventListener(
-        'transitionend',
-        appendContainerImg
-      );
-
-      // 4. Removing transitioned eventlistener when transition ends
-      setTimeout(function () {
-        carousel_featured_properties.removeEventListener(
-          'transitionend',
-          appendContainerImg
-        );
-        carousel_featured_properties.style.transition = `all 0.6s linear`;
-      }, 650);
+      //2. Adding transitionend and removing transitionend
+      addingTransitionEndAndRemovingTransitionEnd();
     }, 3000);
   };
 
+  //==== Function ====//
+  // Adding translate value to move container-image to the left manually with the lef arrow
+  const movingToLeft = () => {
+    leftArrow.addEventListener('click', (ev) => {
+      // 1. Stopping the setInterval
+      clearInterval(slideTransitionDuration);
+      // 2. Starting the transition
+      carousel_featured_properties.style.transform = `translateX(-${gettingContainerImgWidth()}px)`;
+      // 3. Adding transitionend and removing transitionend
+      addingTransitionEndAndRemovingTransitionEnd();
+    });
+  };
+
+  //==== Function ====//
+  // Adding translate value to move container-image to the right manually with the right arrow
+  const movingToRight = () => {
+    rightArrow.addEventListener('click', (ev) => {
+      // 1. Stopping the setInterval
+      clearInterval(slideTransitionDuration);
+      // 2. Starting the transition to the right
+      carousel_featured_properties.style.transform = `translateX(${gettingContainerImgWidth()}px)`;
+    });
+  };
+
   runningCarousel();
+  movingToLeft();
+  movingToRight();
 };
 
 export default carousel;
