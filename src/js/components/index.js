f1c909a3c37380b49aff7d3836f2da17f98701e2
Edwin Scott <scottaguirre@yahoo.com>
2021-01-11 16:32:27 -0600

    branches created

---
+++ src/js/components/index.js
@@ -0,0 +1,11 @@
+import menu from './menu';
+import carousel from './carousel';
+import initialOverlayEffect from './initialOverlayEffect';
+import animateElemInView from './animateElemInView';
+
+export { 
+    menu,
+    carousel,
+    animateElemInView,
+    initialOverlayEffect
+ };
